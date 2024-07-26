073bfaa8a133629ff3141872af980dfbc03de946
rutger <rutgercappendijk@gmail.com>
2024-07-26 12:37:26 +0200

    fixed cnf

--- src/conjunctive_normal_form.rs
+++ src/conjunctive_normal_form.rs
@@ -1,50 +1,31 @@
 use crate::operator::Operator;
 
 impl Operator {
-
-
     fn to_conjunctive_normal_form(self) -> Operator {
-        fn distribute_or(a: &Operator, b: &Operator) -> Operator {
-            match (a, b) {
-                (Operator::And(c, d), e) => {
-                    Operator::and(
-                        distribute_or(c, e),
-                        distribute_or(d, e),
-                    )
-                },
-                (e, Operator::And(c, d)) => {
-                    Operator::and(
-                        distribute_or(e, c),
-                        distribute_or(e, d),
-                    )
-                },
-                _ => Operator::or(a.clone(), b.clone()),
-            }
-        }
         match self {
             Operator::Operand(_) => self,
             Operator::Not(a) => Operator::not(a.to_conjunctive_normal_form()),
+            Operator::And(a, b) => {
+                let a = a.to_conjunctive_normal_form();
+                let b = b.to_conjunctive_normal_form();
+                Operator::And(Box::new(a), Box::new(b))
+            }
             Operator::Or(a, b) => {
                 let a = a.to_conjunctive_normal_form();
                 let b = b.to_conjunctive_normal_form();
-                distribute_or(&a, &b)
+                match (a, b) {
+                    (Operator::And(c, d), e) => Operator::and(
+                        Operator::or(*c, e.clone()).to_conjunctive_normal_form(),
+                        Operator::or(*d, e).to_conjunctive_normal_form(),
+                    ),
+                    (e, Operator::And(c, d)) => Operator::and(
+                        Operator::Or(Box::new(e.clone()), c.clone()).to_conjunctive_normal_form(),
+                        Operator::Or(Box::new(e), d.clone()).to_conjunctive_normal_form(),
+                    ),
+                    (e, f) => Operator::Or(Box::new(e), Box::new(f)),
+                }
             }
-            Operator::And(a, b) => Operator::and(
-                a.to_conjunctive_normal_form(),
-                b.to_conjunctive_normal_form(),
-            ),
-            _ => panic!("This operator should not occur if the formula is in NNF"),
-        }
-    }
-
-    fn to_conjunctive_normal_form_string(self) -> String {
-        let mut result = "";
-        match self {
-            Operator::And(a, b) => format!("{}{}", a.to_conjunctive_normal_form_string(), b.to_conjunctive_normal_form_string()),
-            Operator::Or(a, b) => format!("{}{}|", a.to_conjunctive_normal_form_string(), b.to_conjunctive_normal_form_string()),
-            Operator::Not(c) => format!("{}!", c.to_conjunctive_normal_form_string()),
-            Operator::Operand(c) => c.to_string(),
-            _ => panic!("This operator should not occur if the formula is in NNF"),
+            a => panic!("Operator {:?} not allowed. Expression must be in NNF", a),
         }
     }
 }
@@ -94,21 +75,30 @@ mod tests {
     }
 
     #[test]
+    fn conjunctive_normal_form_works_with_1() {
+        let result = conjunctive_normal_form("ABCD&|&");
+
+        assert_eq!(result, "ABC|BD|&&");
+    }
+
+    #[test]
     fn conjunctive_normal_form_works_with_many_ors() {
         let result = conjunctive_normal_form("AB|C|D|");
 
-        assert_eq!(result, "ABCD|||");
+        assert_eq!(result, "AB|C|D|");
     }
 
-        // Or(Operand('A'), Or(Operand('B'), Or(Operand('C'), Operand('D'))))
-        // Or(Or(Or(Operand('A'), Operand('B')), Operand('C')), Operand('D'))
     #[test]
-    fn test() {
-        let result = Operator::from_formula("AB|C|D|").unwrap();
+    fn conjunctive_normal_form_works_with_many_ands() {
+        let result = conjunctive_normal_form("AB&C&D&");
 
-        let cnf = result.clone().to_conjunctive_normal_form();
-        let cnf_string = cnf;
-        println!("{:?}", cnf_string);
-        assert_eq!(cnf_string.to_string(), "ABCD|||");
+        assert_eq!(result, "AB&C&D&");
+    }
+
+    #[test]
+    fn conjunctive_normal_form_works_complicated() {
+        let result = conjunctive_normal_form("AB&!C!|");
+
+        assert_eq!(result, "A!B!|C!|");
     }
 }
--- src/sat.rs
+++ src/sat.rs
@@ -1,4 +1,11 @@
+use crate::operator::Operator;
+
 pub fn sat(formula: &str) -> bool {
+    let operator = Operator::from_formula(formula);
+    if operator.is_none() {
+        return true;
+    }
+    let operator = operator.unwrap();
     todo!()
 }
 
@@ -7,7 +14,12 @@ mod tests {
     use super::*;
 
     #[test]
+    fn empty_clause_is_satisfiable() {
+        assert_eq!(sat(""), false);
+    }
+
+    #[test]
     fn test_sat() {
-        // Test cases here
+        assert_eq!(sat("AB|"), true);
     }
 }
